80a62a78c1a7ec68c1c2ce35134299fa23dd1405
HoangTien0105 <hoangtien01052003@gmail.com>
2025-03-01 00:06:48 +0700

    add get pricing model endpoints

---
+++ SEP490.AffiliateNetwork/ANF.Application/Controllers/v1/PricingModelController.cs
@@ -0,0 +1,51 @@
+﻿using ANF.Core;
+using ANF.Core.Commons;
+using ANF.Core.Models.Entities;
+using ANF.Core.Models.Responses;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ANF.Application.Controllers.v1
+{
+    public class PricingModelController : BaseApiController
+    {
+        /// <summary>
+        /// Get all pricing models
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("pricing-models")]
+        [MapToApiVersion(1)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetPricingModels()
+        {
+            var pricingModels = PricingModelConstant.pricingModels;
+            return Ok(new ApiResponse<List<PricingModel>>
+            {
+                IsSuccess = true,
+                Message = "Success.",
+                Value = pricingModels
+            });
+        }
+
+        /// <summary>
+        /// Get pricing model by id
+        /// </summary>
+        /// <param name="id">Pricing model id</param>
+        /// <returns></returns>
+        [HttpGet("pricing-models/{id}")]
+        [MapToApiVersion(1)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetPricingModel(long id)
+        {
+            var pricingModel = PricingModelConstant.pricingModels.Where(e => e.Id == id).FirstOrDefault();
+            return Ok(new ApiResponse<PricingModel>
+            {
+                IsSuccess = true,
+                Message = "Success.",
+                Value = pricingModel
+            });
+        }
+    }
+}
---
+++ SEP490.AffiliateNetwork/ANF.Core/PricingModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANF.Core
+{
+    public class PricingModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public static class PricingModelConstant
+    {
+        public static readonly List<PricingModel> pricingModels = new()
+    {
+        new PricingModel { Id = 1, Name = "CPC", Description = "Cost per click" },
+        new PricingModel { Id = 2, Name = "CPV", Description = "Cost per view" },
+        new PricingModel { Id = 3, Name = "CPA", Description = "Cost per action" },
+    };
+    }
+
+}
