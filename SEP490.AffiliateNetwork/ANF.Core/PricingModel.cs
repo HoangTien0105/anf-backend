c809ebeec653d99418d2f182a169a97f907a0d51
HoangTien0105 <hoangtien01052003@gmail.com>
2025-03-01 07:30:38 +0700

    [WIP] Modify pricing model constant

--- SEP490.AffiliateNetwork/ANF.Core/PricingModel.cs
+++ SEP490.AffiliateNetwork/ANF.Core/PricingModel.cs
@@ -19,7 +19,7 @@ namespace ANF.Core
     {
         new PricingModel { Id = 1, Name = "CPC", Description = "Cost per click" },
         new PricingModel { Id = 2, Name = "CPV", Description = "Cost per view" },
-        new PricingModel { Id = 3, Name = "CPA", Description = "Cost per action" },
+        new PricingModel { Id = 3, Name = "CPI", Description = "Cost per install" },
     };
     }
 
