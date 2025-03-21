2575aa69d26dce354a03a7c3b6dfa974a54aa92b
Vũ Long <vlongbd.213@gmail.com>
2025-03-21 19:56:48 +0700

    add properties in Offer

---
+++ SEP490.AffiliateNetwork/ANF.Core/Enums/OfferStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace ANF.Core.Enums
+{
+    public enum OfferStatus
+    {
+        Pending = 0,
+        Approved = 1,
+        Rejected = 2,
+        Started = 3,
+        Ended = 4,
+    }
+}
---
+++ SEP490.AffiliateNetwork/ANF.Infrastructure/Migrations/20250321125355_AddProperties.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ANF.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "rejected_reason",
+                table: "Offers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "status",
+                table: "Offers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "rejected_reason",
+                table: "Offers");
+
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "Offers");
+        }
+    }
+}
