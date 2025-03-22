c43a350fd2619739a412004f6f1b1a0f63af59f7
Vũ Long <vlongbd.213@gmail.com>
2025-03-23 01:09:14 +0700

    fix login responded model and publisher offer response

--- SEP490.AffiliateNetwork/ANF.Core/Models/Responses/LoginResponse.cs
+++ SEP490.AffiliateNetwork/ANF.Core/Models/Responses/LoginResponse.cs
@@ -2,6 +2,28 @@
 {
     public class LoginResponse
     {
+        public long Id { get; set; }
+
+        public string UserCode { get; init; } = null!;
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? CitizenId { get; set; }
+
+        public string? Address { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Role { get; set; } = null!;
+
+        public string? ImageUrl { get; set; }
+
         public string AccessToken { get; set; } = null!;
     }
 }
