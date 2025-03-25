89dba21febc339affffa55e4b9f622ac7874e029
HoangTien0105 <hoangtien01052003@gmail.com>
2025-03-25 18:49:08 +0700

    add get all pending and verify offer by publisher

--- SEP490.AffiliateNetwork/ANF.Core/Models/Responses/CampaignPubDetailedResponse.cs
+++ SEP490.AffiliateNetwork/ANF.Core/Models/Responses/CampaignPubDetailedResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ANF.Core.Models.Responses
+﻿namespace ANF.Core.Models.Responses
 {
     public class CampaignPubDetailedResponse
     {
