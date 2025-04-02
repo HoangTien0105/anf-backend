1df7c19e2268c063b23a3eb54a57b52956462913
Vũ Long <vlongbd.213@gmail.com>
2025-04-02 21:42:52 +0700

    finish publish valid tracking data into queue

---
+++ SEP490.AffiliateNetwork/ANF.Core/Models/Responses/ConversionResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace ANF.Core.Models.Responses
+{
+    /// <summary>
+    /// Model to get the stored data from queue (RabbitMQ)
+    /// </summary>
+    public class ConversionResponse
+    {
+        /// <summary>
+        /// Validation's id from tracking validation table
+        /// </summary>
+        public long Id { get; set; }
+
+        public string ClickId { get; set; } = null!;
+
+        public string PublisherCode { get; set; } = null!;
+
+        public long OfferId { get; set; }
+
+        public string PricingModel { get; set; } = null!;
+
+        public int MyProperty { get; set; }
+
+        public decimal Revenue { get; set; }
+    }
+}
