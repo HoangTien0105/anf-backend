89b735b46726d9502c134bf8c29733e319f5ba05
dkmtech32 <dkmtien123@gmail.com>
2025-04-13 14:31:03 +0000

    add virtual host to RabbitMQPublisher

--- SEP490.AffiliateNetwork/ANF.Service/RabbitMQPublisher.cs
+++ SEP490.AffiliateNetwork/ANF.Service/RabbitMQPublisher.cs
@@ -18,7 +18,8 @@ namespace ANF.Service
             {
                 HostName = options.Value.Host,
                 UserName = options.Value.Username,
-                Password = options.Value.Password
+                Password = options.Value.Password,
+                VirtualHost = options.Value.VirtualHost,
             };
             _connection = factory.CreateConnectionAsync().GetAwaiter().GetResult();
             _channel = _connection.CreateChannelAsync().Result;
