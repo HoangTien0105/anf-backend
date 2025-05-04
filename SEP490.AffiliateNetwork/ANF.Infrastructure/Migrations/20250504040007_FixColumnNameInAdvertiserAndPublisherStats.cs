0ca302e3a95a54820157ca335a5897be6a7b5bb2
Vũ Long <vlongbd.213@gmail.com>
2025-05-04 11:11:51 +0700

    fix column name

---
+++ SEP490.AffiliateNetwork/ANF.Infrastructure/Migrations/20250504040007_FixColumnNameInAdvertiserAndPublisherStats.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ANF.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixColumnNameInAdvertiserAndPublisherStats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PublisherOfferStats",
+                table: "PublisherOfferStats");
+
+            migrationBuilder.RenameTable(
+                name: "PublisherOfferStats",
+                newName: "PublisherCampaignStats");
+
+            migrationBuilder.RenameColumn(
+                name: "total_phone",
+                table: "AdvertiserOfferStats",
+                newName: "total_computer");
+
+            migrationBuilder.RenameColumn(
+                name: "total_phone",
+                table: "PublisherCampaignStats",
+                newName: "total_computer");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PublisherCampaignStats",
+                table: "PublisherCampaignStats",
+                column: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PublisherCampaignStats",
+                table: "PublisherCampaignStats");
+
+            migrationBuilder.RenameTable(
+                name: "PublisherCampaignStats",
+                newName: "PublisherOfferStats");
+
+            migrationBuilder.RenameColumn(
+                name: "total_computer",
+                table: "AdvertiserOfferStats",
+                newName: "total_phone");
+
+            migrationBuilder.RenameColumn(
+                name: "total_computer",
+                table: "PublisherOfferStats",
+                newName: "total_phone");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PublisherOfferStats",
+                table: "PublisherOfferStats",
+                column: "id");
+        }
+    }
+}
