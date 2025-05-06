7b2ee7b2d2e17e44512f8a47f2af63103eb06f84
Hoang Tien <hoangtien01052003@gmail.com>
2025-05-06 15:37:51 +0700

    get postbacks by offer's id

--- SEP490.AffiliateNetwork/ANF.Core/Services/IPostbackService.cs
+++ SEP490.AffiliateNetwork/ANF.Core/Services/IPostbackService.cs
@@ -1,5 +1,6 @@
 ﻿using ANF.Core.Models.Entities;
 using ANF.Core.Models.Requests;
+using ANF.Core.Models.Responses;
 
 namespace ANF.Core.Services
 {
@@ -10,6 +11,7 @@ namespace ANF.Core.Services
         public Task<bool> UpdatePostBackStatus(long id, string status);
         public Task<bool> UpdatePostBackLog(string id, PostbackLogUpdateRequest request);
         public Task<PostbackData> GetPostbackDataByTransactionId(string id);
+        public Task<PaginationResponse<PostbackData>> GetAllPostbackOfferId(long id, PaginationRequest request);
         public Task<List<PurchaseLog>> GetAllPostbackLogByClickId(string id);
         public Task<List<PurchaseLog>> GetAllPostbackLogByTransactionId(string id);
         public Task<PurchaseLog> GetPostbackLogById(long id);
