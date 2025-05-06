3b2b7215ef336b60d39d0561518726a027cf8a15
Vũ Long <vlongbd.213@gmail.com>
2025-05-06 16:20:23 +0700

    add background service for updating data for statistics

--- SEP490.AffiliateNetwork/ANF.Application/Extensions/ServiceCollectionExtensions.cs
+++ SEP490.AffiliateNetwork/ANF.Application/Extensions/ServiceCollectionExtensions.cs
@@ -72,6 +72,7 @@ namespace ANF.Application.Extensions
             services.AddHostedService<CampaignBackgroundService>();
             services.AddHostedService<StatisticBackgroundService>();
             services.AddHostedService<AdminStatsBackgroundService>();
+            services.AddHostedService<AdvertiserCampaignStatsBackgroundService>();
             #endregion
 
             services.Configure<ForwardedHeadersOptions>(o =>
@@ -270,6 +271,7 @@ namespace ANF.Application.Extensions
             services.AddSingleton<StatisticBackgroundService>();
             services.AddSingleton<CampaignBackgroundService>();
             services.AddSingleton<AdminStatsBackgroundService>();
+            services.AddSingleton<AdvertiserCampaignStatsBackgroundService>();
             #endregion
             return services;
         }
---
+++ SEP490.AffiliateNetwork/ANF.Service/Backgrounds/AdvertiserCampaignStatsBackgroundService.cs
@@ -0,0 +1,152 @@
+﻿using ANF.Core;
+using ANF.Core.Enums;
+using ANF.Core.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ANF.Service.Backgrounds
+{
+    public class AdvertiserCampaignStatsBackgroundService(
+        IServiceScopeFactory serviceScopeFactory,
+        ILogger<AdvertiserCampaignStatsBackgroundService> logger) : BackgroundService
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory = serviceScopeFactory;
+        private readonly ILogger<AdvertiserCampaignStatsBackgroundService> _logger = logger;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    // Calculate the delay until the next 23:50
+                    var now = DateTime.Now;
+                    var nextRunTime = DateTime.Today.AddHours(23).AddMinutes(50);
+
+                    // If the current time is already past 23:50, schedule for the next day
+                    if (now > nextRunTime)
+                    {
+                        nextRunTime = nextRunTime.AddDays(1);
+                    }
+
+                    var delay = nextRunTime - now;
+
+                    // Wait until the next run time or until the task is canceled
+                    await Task.Delay(delay, stoppingToken);
+
+                    using var scope = _serviceScopeFactory.CreateScope();
+                    var unitOfWork = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+                    await AddStatsDataForAdvertiser(unitOfWork);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e.Message, e.StackTrace);
+                    await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Update advertiser's statistics data by day
+        /// </summary>
+        /// <param name="unitOfWork"></param>
+        /// <returns></returns>
+        private async Task AddStatsDataForAdvertiser(IUnitOfWork unitOfWork)
+        {
+            var today = DateTime.UtcNow.Date;
+            var endTime = DateTime.UtcNow;
+            if (endTime.Hour == 23 && endTime.Minute >= 50)
+            {
+                endTime = new DateTime(today.Year, today.Month, today.Day, 23, 50, 0);
+            }
+            var trackingEventRepo = unitOfWork.GetRepository<TrackingEvent>();
+            var campaignRepo = unitOfWork.GetRepository<Campaign>();
+            var publisherOfferRepo = unitOfWork.GetRepository<PublisherOffer>();
+            var advertiserCampaignStatsRepo = unitOfWork.GetRepository<AdvertiserCampaignStats>();
+
+            // Initialize device type counters
+            int totalMobile = 0, totalComputer = 0, totalTablet = 0;
+
+            // TODO: Có cần lấy dữ liệu thống kê cho các campaign có state là "Verified" không?
+            var campaigns = await campaignRepo.GetAll()
+                .AsNoTracking()
+                .Include(c => c.Offers)
+                .Where(c => c.Status == CampaignStatus.Started)
+                .ToListAsync();
+
+            foreach (var campaign in campaigns)
+            {
+                var offerIds = campaign.Offers.Select(o => o.Id).ToList();
+                var trackingEvents = await trackingEventRepo.GetAll()
+                    .AsNoTracking()
+                    .Include(te => te.TrackingValidation)
+                    .Where(te => te.ClickTime >= today && te.ClickTime <= endTime
+                        && offerIds.Contains(te.OfferId))
+                    .ToListAsync();
+
+                var totalClick = trackingEvents.Count();
+
+                // Verified Clicks
+                var totalVerifiedClick = trackingEvents
+                    .Where(te => te.Status == TrackingEventStatus.Valid &&
+                        te.TrackingValidation.ValidationStatus == ValidationStatus.Success)
+                    .Count();
+
+                // Fraud Clicks
+                var totalFraudClick = trackingEvents
+                    .Where(te => te.Status == TrackingEventStatus.Fraud)
+                    .Count();
+
+                // Thống kê gom lại của các ngày trước đó cho đến thời điểm hiện tại
+                var totalJoinedPublisher = await publisherOfferRepo.GetAll()
+                    .AsNoTracking()
+                    .Where(po => po.Offer.CampaignId == campaign.Id
+                        && po.Status == PublisherOfferStatus.Approved)
+                    .Select(po => po.PublisherCode)
+                    .Distinct()
+                    .CountAsync();
+
+                // Thống kê gom lại của các ngày trước đó cho đến thời điểm hiện tại
+                var totalRejectedPublisher = await publisherOfferRepo.GetAll()
+                    .AsNoTracking()
+                    .Where(po => po.Offer.CampaignId == campaign.Id
+                        && po.Status == PublisherOfferStatus.Rejected)
+                    .Select(po => po.PublisherCode)
+                    .Distinct()
+                    .CountAsync();
+                
+                // Budget đã chi của campaign
+                var spendingBudget = campaign.Budget - campaign.Balance;
+
+                // DeviceDetectorNET
+                foreach (var item in trackingEvents)
+                {
+                    var dd = new DeviceDetectorNET.DeviceDetector(item.UserAgent);
+                    dd.Parse();
+
+                    if (dd.IsMobile()) totalMobile++;
+                    else if (dd.IsTablet()) totalTablet++;
+                    else totalComputer++;
+                }
+                var advertiserCampaignStats = new AdvertiserCampaignStats
+                {
+                    CampaignId = campaign.Id,
+                    Date = endTime, // Operation time
+                    TotalClick = totalClick,
+                    TotalVerifiedClick = totalVerifiedClick,
+                    TotalFraudClick = totalFraudClick,
+                    TotalJoinedPublisher = totalJoinedPublisher,
+                    TotalRejectedPublisher = totalRejectedPublisher,
+                    TotalBudgetSpent = (decimal)spendingBudget,
+                    TotalMobile = totalMobile,
+                    TotalComputer = totalComputer,
+                    TotalTablet = totalTablet
+                };
+                advertiserCampaignStatsRepo.Add(advertiserCampaignStats);
+            }
+            await unitOfWork.SaveAsync();
+        }
+    }
+}
